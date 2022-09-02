816fc822cd906886f04c1345d93cadab0d525141
SAI <SAI@DESKTOP-HOUS858>
2022-09-02 22:12:45 +0530

    running java program

--- src/demo1/Sunita1.java
+++ src/demo1/Sunita1.java
@@ -1,11 +1,10 @@
 package demo1;
 
-public class Sunita {
+public class Sunita1 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		System.out.println("Running java program");
-
-	}
+}
 
 }
