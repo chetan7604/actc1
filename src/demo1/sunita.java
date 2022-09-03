8fa59f034a5e34c92a5d8d991e85f197c11d5f47
SAI <SAI@DESKTOP-HOUS858>
2022-09-03 14:48:55 +0530

    Merge branch 'master' of https://github.com/chetan7604/actc1.git Array practice

--- src/demo1/sunita.java
+++ src/demo1/sunita.java
@@ -2,9 +2,23 @@ package demo1;
 
 public class sunita {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+	
+		String ar[]=new String[5];
+		ar[0]="X";
+		ar[1]="Y";
+		ar[2]="Z";
+		ar[3]="A";
+		ar[4]="B";
+		System.out.println(ar[0]);
+		System.out.println(ar[1]);
+		System.out.println(ar[2]);
+		System.out.println(ar[3]);
+		System.out.println(ar[4]);
+		
 
+	
 	}
 
 }
---
+++ src/demo1/sunita2.java
@@ -0,0 +1,17 @@
+package demo1;
+
+public class sunita2 {
+
+	public static void main(String[] args) 
+	{
+		int ar[]=new int[3];
+		ar[0]=100;
+		ar[1]=200;
+		ar[2]=300;
+		System.out.println(ar[0]);
+		System.out.println(ar[1]);
+		System.out.println(ar[2]);
+
+	}
+
+}
