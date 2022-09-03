abc53d00373e35c190ad675ac66b1a2f3a4df161
CDAC-pratiksha <pratikshamandlik24@gmail.com>
2022-09-03 11:22:46 +0530

    second code

---
+++ src/demo1/Starpattern.java
@@ -0,0 +1,21 @@
+package demo1;
+
+public class Starpattern
+{
+	public static void main(String[] args) 
+	{
+		int star=1;
+		
+		for(int i=1;i<=10;i++)
+		{
+			for(int j=1;j<=star;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+			star++;
+		}
+		
+	}
+
+}
