f20c136be2e48eb5cb79b4137ede5d10af4b075e
SAI <SAI@DESKTOP-HOUS858>
2022-09-03 14:33:39 +0530

    STRING PRACTICE

--- src/demo1/SUNITA_1.java
+++ src/demo1/SUNITA_1.java
@@ -2,9 +2,10 @@ package demo1;
 
 public class SUNITA_1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
 
+        System.out.println("my first code");
 	}
 
 }
