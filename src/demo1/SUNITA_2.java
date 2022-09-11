7fb8f351054bf948e1328c8ba71e8f11869164c8
SAI <SAI@DESKTOP-HOUS858>
2022-09-11 08:42:34 +0530

    MY FIRST LOGICAL PROGRAM#

---
+++ src/demo1/SUNITA_2.java
@@ -0,0 +1,22 @@
+package demo1;
+
+public class SUNITA_2 
+{
+
+	public static void main(String[] args)
+	{
+		 for(int i=0;i<=26;i++)
+		   {
+			   char ch='A';
+			   for(int j=1;j<=i;j++)
+			   {
+				   System.out.print(ch+" ");
+				   ch++;
+			   }
+			   System.out.println();
+			   
+		   }
+
+	}
+
+}
