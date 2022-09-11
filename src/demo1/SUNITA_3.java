8168506d35f5ecc311a0a23ea60bf3d30cf63c2d
SAI <SAI@DESKTOP-HOUS858>
2022-09-11 08:54:01 +0530

    COLLECTION PRACTICE TREESET

--- src/demo1/SUNITA_3.java
+++ src/demo1/SUNITA_3.java
@@ -1,9 +1,27 @@
 package demo1;
 
-public class SUNITA_3 {
+import java.util.TreeSet;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class SUNITA_3 
+{
+
+	public static void main(String[] args) 
+	{
+		TreeSet ts=new TreeSet();
+		ts.add(10);
+		ts.add(20);
+		ts.add(5);
+		ts.add(60);
+		ts.add(70);
+		ts.add(40);
+		ts.add(100);
+		System.out.println(ts);
+		ts.remove(40);
+		System.out.println(ts);
+		System.out.println(ts.size());
+		System.out.println(ts.contains(5));
+
+	
 
 	}
 
