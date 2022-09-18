f464cca6ff7184d72af73fb2c7d2689ffba7d369
hp <hp@DESKTOP-L54AJ1J>
2022-09-18 11:49:06 +0530

    vowel

--- src/demo1/vowelcount.java
+++ src/demo1/vowelcount.java
@@ -2,4 +2,30 @@ package demo1;
 
 public class vowelcount {
 
+	
+	public static void main(String[] args) {
+		
+		 String S = "welcome india";
+		 
+		char[]letters= S.toCharArray();
+		int count=0;
+		
+		for(char c: letters)
+		{
+			switch(c)
+			{
+			case'a' :
+			case 'e' :
+			case 'i' :
+			case 'o' :
+			case 'u' :
+			 count++;
+			 break;
+			 default :
+			}
+		}
+		System.out.println("total vowels in string=" + count);	
+	}
 }
+
+
