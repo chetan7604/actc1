c9fad6ead2f29857ff004635e584b8c6e81a7189
narhari273 <manesachin273@gmail.com>
2023-04-23 15:23:06 +0530

    mane program

---
+++ src/ABC273/STAR_MAGIC.java
@@ -0,0 +1,19 @@
+package ABC273;
+
+public class STAR_MAGIC
+{
+
+	public static void main(String[] args) 
+	{
+		int star= 4;
+		for(int i=1;i<=4;i++) 
+		{
+			for(int j=1;j<=star;j++)
+			{
+				System.out.print("* ");
+			}
+		System.out.println();
+		star--;
+		}
+	}
+}
